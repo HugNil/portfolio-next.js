df29f57a439db69f95e754c67bee3361ef77235d
HugNil <hugo.nilsson0002@gmail.com>
2026-04-20 13:21:35 +0200

    Added based in Skåne, Sweden to hero section

--- frontend/src/app/components/Hero.jsx
+++ frontend/src/app/components/Hero.jsx
@@ -59,7 +59,7 @@ export default function Hero() {
               STUDENT
             </h1>
           </div>
-          <p className={styles.kicker}>Portfolio / Kristianstad University</p>
+          <p className={styles.kicker}>Based in Skåne, Sweden</p>
           <div className={styles.buttonContainer}>
             <a href="/assets/Hugo Nilsson CV.pdf" onClick={handleDownloadCV}>
               <button className="button-primary" style={{minWidth: '8rem'}}>RESUME</button>
